9d24c222bd7c2a67f0a587842bca13d5928d2c10
Yves62 <boulinguez.yves@orange.fr>
2022-12-11 22:07:37 +0100

    feat: added code in app.js file and some modification in html and css code

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-const userName = document.querySelector("#user")
+const userName = document.querySelector("#user");
 const form = document.querySelector("#form");
-const profilName = document.querySelector('.name');
-const repo = document.querySelector('.repo');
-const bio = document.querySelector('.bio');
-const imgUser = document.querySelector('img');
+const profilName = document.querySelector(".name");
+const repo = document.querySelector(".repo");
+const bio = document.querySelector(".bio");
+const imgUser = document.querySelector("img");
 let url = `https://api.github.com/users/`;
 
+window.addEventListener("load", () => {
+  url = `https://api.github.com/users/Yves62`;
+  handleUser();
+});
 
+userName.addEventListener("input", (e) => {
+  url = `https://api.github.com/users/${e.target.value}`;
+  handleUser();
+});
 
+async function handleUser() {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    } else {
+      const data = await response.json();
+      handleInfo(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function handleInfo(data) {
+  imgUser.src = data.avatar_url;
+  profilName.textContent = `Nom : ${data.name}`;
+  repo.textContent = `Repos : ${data.public_repos}`;
+  bio.textContent = `Bio : ${data.bio}`;
+}
