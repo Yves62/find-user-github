fe63b7b0d938ca6b2febeff9d714798cca397c4d
Yves62 <boulinguez.yves@orange.fr>
2022-12-11 21:50:07 +0100

    feat: added const in app.js file

---
+++ app.js
@@ -0,0 +1,10 @@
+const userName = document.querySelector("#user")
+const form = document.querySelector("#form");
+const profilName = document.querySelector('.name');
+const repo = document.querySelector('.repo');
+const bio = document.querySelector('.bio');
+const imgUser = document.querySelector('img');
+let url = `https://api.github.com/users/`;
+
+
+
